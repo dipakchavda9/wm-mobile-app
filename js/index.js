8499864fd9f830baafbeada1297de283213993b4
wadhwanadmin <wadhwanmandir@gmail.com>
2017-07-01 17:54:24 +0530

    Adds functionality for receiving notification into application.

--- js/index.js
+++ js/index.js
@@ -16,6 +16,12 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var baseURL = 'http://wadhwanmandir.org/api/public/';
+var authorizationType = 'Basic';
+var authorizationHash = 'd2FkaHdhbm1hbmRpcmFwaTpXNGRodyRuYXBpITEwMDg=';
+var storage = window.localStorage;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,11 +40,106 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        
+        $('body').height(($(window).height() * 0.9));
+        registerPushNotification();
+        return;
     },
-    
     exitAppFunction: function() {
         navigator.app.exitApp();
     },
-    // Update DOM on a Received Event
+
 };
+
+app.initialize();
+
+function registerPushNotification() {
+
+    var push = PushNotification.init({ "android": {"senderID": "476875120878"} });
+
+    push.on('registration', function(data) {
+        storeRegIdToServer(data.registrationId);
+    });
+
+    push.on('notification', function(data) {
+        alert("Notification Message: " + data.message)
+    // data.message,
+    // data.title,
+    // data.count,
+    // data.sound,
+    // data.image,
+    // data.additionalData
+    });
+
+    push.on('error', function(e) {
+        // alert("Notification Error: " + e.message);
+        alert("There was some error in receiving notifications, please report this problem via 'Contact Us'.");
+    });
+
+}
+
+function storeRegIdToServer(reg_id) {
+    // alert("Storing Registration Id to Server: " + reg_id);
+    var existingRegId = storage.getItem('Notification-Registration-Id');
+    var jsonData = {
+        "reg_id" : reg_id
+    };
+
+    if(existingRegId) {
+        if(existingRegId == reg_id) {
+            // alert("Registration Id already stored on server.");
+        } else {
+            // alert("Updating Registration Id to server.");
+            var notificationTableId = storage.getItem("Notification-Table-Id");
+            $.ajax({
+                type: 'PATCH',
+                url: baseURL + 'notification/' + notificationTableId,
+                contentType: 'application/json',
+                dataType: 'json',
+                data: JSON.stringify(jsonData),
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Accept', 'application/json');
+                    xhr.setRequestHeader('Content-Type', 'application/json');
+                    xhr.setRequestHeader('Authorization', authorizationType + ' ' + authorizationHash);
+                }
+            })
+            .done((data, textStatus, jqXHR) => {
+                // alert("Registration key updated successfully!");
+                storage.setItem("Notification-Table-Id", data.id);
+                storage.setItem("Notification-Registration-Id", reg_id);
+            })
+            .fail((xhr, textStatus, errorThrown) => {
+                alert("There was some error in receiving notifications, please report this problem via 'Contact Us'.");
+                // alert("readyState: " + xhr.readyState + "\nstatus: " + xhr.status);
+                // alert("responseText: " + xhr.responseText);
+                // alert("textStatus: " + textStatus);
+                // alert("errorThrown: " + errorThrown);
+            });
+        }
+    } else {
+        // alert("Inserting Registration Id to server.");
+        $.ajax({
+            type: 'POST',
+            url: baseURL + 'notification',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify(jsonData),
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Accept', 'application/json');
+                xhr.setRequestHeader('Content-Type', 'application/json');
+                xhr.setRequestHeader('Authorization', authorizationType + ' ' + authorizationHash);
+            }
+        })
+        .done((data, textStatus, jqXHR) => {
+            // alert("Registration key inserted successfully!");
+            storage.setItem("Notification-Table-Id", data.id);
+            storage.setItem("Notification-Registration-Id", reg_id);
+        })
+        .fail((xhr, textStatus, errorThrown) => {
+            alert("There was some error in receiving notifications, please report this problem via 'Contact Us'.");
+            // alert("readyState: " + xhr.readyState + "\nstatus: " + xhr.status);
+            // alert("responseText: " + xhr.responseText);
+            // alert("textStatus: " + textStatus);
+            // alert("errorThrown: " + errorThrown);
+        });
+    }
+}
