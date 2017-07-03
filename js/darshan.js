72ad7ab0234667a3fe0bca73422d3d41c12d1f88
wadhwanadmin <wadhwanmandir@gmail.com>
2017-07-03 17:46:39 +0530

    Adds date wise darshan functionality.

--- js/darshan.js
+++ js/darshan.js
@@ -1,30 +1,49 @@
-var errorCount = 0;
-var imageCount = 25;
+var errorCount;
+var imageCount;
+var triedLoadingImageCount;
 
-function displayTodaysDarshan() {
+function displayDarshan(date = null) {
 
-    var dateObj = new Date();
-    var dd = dateObj.getDate();
-    var mm = dateObj.getMonth()+1;
-    var yyyy = dateObj.getFullYear(); 
-    if(dd<10) {
-        dd='0'+dd;
-    } 
-    if(mm<10) {
-        mm='0'+mm;
+    errorCount = 0;
+    imageCount = 25;
+    triedLoadingImageCount = 0;
+
+    $('#datepickerRow').hide();
+    $('#message').hide();
+    $('#darshan').show();
+
+    if(!date) {
+        var dateObj = new Date();
+        var dd = dateObj.getDate();
+        var mm = dateObj.getMonth()+1;
+        var yyyy = dateObj.getFullYear(); 
+        if(dd<10) {
+            dd='0'+dd;
+        } 
+        if(mm<10) {
+            mm='0'+mm;
+        }
+        date = dd+'-'+mm+'-'+yyyy;
+
+        $('#message').html(`<h2>Jay Swaminarayan!<br/><br/>Today's Darshan will be displayed soon!</h2>`);
+
+    } else {
+
+        $('#message').html(`<h2>Jay Swaminarayan!<br/><br/>Sorry...<br/>Darshan of date '` + date + `' is not available.</h2>`);
+
     }
-    date = dd+'-'+mm+'-'+yyyy;
+
+    $('#titleDate').html(date);
 
     var screenHeight = $(window).height();
     var screenWidth = $(window).width();
 
-    var darshanHtml = '<div id="lightgallery">';
+    var darshanHtml = '';
     var i;
     for(i=1;i<=imageCount;i++) {
         darshanHtml += '<a href="http://wadhwanmandir.org/wp-content/uploads/dailydarshan/dev' + i + '_' + date + '.jpg" data-download-url="http://wadhwanmandir.org/wp-content/uploads/dailydarshan/dev' + i + '_' + date + '.jpg"><img src="http://wadhwanmandir.org/wp-content/uploads/dailydarshan/dev' + i + '_' + date + '.jpg" max-height="' + screenHeight/2 + 'px" max-width="' + screenWidth/2 + 'px" height="50%" width="50%" onerror="removeImage(this)" onload="addStyletoImage(this)"/></a>';
     }
-    darshanHtml += '</div>';
-    $("#darshan").html(darshanHtml);
+    $("#lightgallery").html(darshanHtml);
 
     lightGallery(document.getElementById('lightgallery'), {
         mode: 'lg-fade',
@@ -39,15 +58,24 @@ function removeImage(img) {
     if(errorCount >= imageCount) {
         showMessageBlock();
     }
+    increamentTriedLoadingImageCount();
 }
 
 function addStyletoImage(img) {
     $(img).css('border', '2px solid white');
+    increamentTriedLoadingImageCount();
 }
 
 function showMessageBlock() {
     $('#message').show();
     $('#darshan').hide();
+}
+
+function increamentTriedLoadingImageCount() {
+    triedLoadingImageCount++;
+    if(triedLoadingImageCount >= imageCount) {
+        $('#datepickerRow').show();
+    }
 }
 
 /*
